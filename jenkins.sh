45bc72be596db89dae1006a6ef4eb1854f48ef56
vshndevops <136984642+vshndevops@users.noreply.github.com>
2023-09-26 17:52:16 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add Jenkins repository key to keyring
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Add Jenkins repository to sources.list.d
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update the package list
+sudo apt-get update
+
+# Install Jenkins
+sudo apt-get install jenkins
